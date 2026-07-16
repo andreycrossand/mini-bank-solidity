182c9f6202cca3187ea36966c84c64bf69c09925
Andrey Glyva <aglyvav@gmail.com>
2026-07-17 00:34:14 +0300

    several new features and bags were fixed

--- contracts/miniBank.sol
+++ contracts/miniBank.sol
@@ -9,6 +9,8 @@ contract MyMiniBank {
     error UserHasNotRegisteredYet();
     error NotEnoughMoney();
     error AmountTooSmall();
+    error TransferFailed();
+    error TimeLocked();
 
     event Deposit(address user, uint256 amount);
     event Withdraw(address user, uint256 amount);
@@ -26,6 +28,9 @@ contract MyMiniBank {
 
     ///@notice address => ID to address
     mapping (uint256 => address) public idToUserAddress;
+
+    ///@notice ID to time last deposit
+    mapping (uint256 => uint256) public depositTime;
     
     constructor() {
         owner = msg.sender;
@@ -46,6 +51,13 @@ contract MyMiniBank {
         idToUserAddress[nextId] = msg.sender;
         userAddressToID[msg.sender] = nextId;
         nextId++;
+    }
+
+    function claimProfit() public onlyOwner {
+        uint256 currentTotalProfit = totalProfit;
+        totalProfit = 0;
+        (bool success, ) = payable(owner).call{value: currentTotalProfit}("");
+        require(success, TransferFailed());
     }
 
     function getBalance(uint256 _id) public view returns(uint256) {
@@ -69,18 +81,24 @@ contract MyMiniBank {
     function deposit() public payable {
         uint256 currentId = userAddressToID[msg.sender];
         require(currentId != 0, UserHasNotRegisteredYet());
+        if (depositTime[currentId] != 0) {
+            require(block.timestamp >= 10 minutes + depositTime[currentId], TimeLocked());
+        }
         userBalance[currentId] += msg.value;
+        depositTime[currentId] = block.timestamp;
         emit Deposit(msg.sender, msg.value);
     }
 
-    ///@notice withdraw with bank fee (5 %)
+    ///@dev withdraw with bank fee (5 %)
     function withdraw(uint256 _amount) public {
         uint256 profit = 0;
         uint256 newCurrentId = userAddressToID[msg.sender]; 
         require(newCurrentId != 0, UserHasNotRegisteredYet());
         require(_amount <= userBalance[newCurrentId], NotEnoughMoney());
         require(_amount >= 20, AmountTooSmall());
-
+        if(depositTime[newCurrentId] != 0) {
+            require(block.timestamp >= 10 seconds + depositTime[newCurrentId], TimeLocked());
+        }
         if (userBalance[newCurrentId] >= VIP_THRESHOLD) {
             profit = _amount / 100;
         }
@@ -88,11 +106,11 @@ contract MyMiniBank {
         else {
             profit = _amount / 20;
         }
-
         userBalance[newCurrentId] -= _amount;
         totalProfit += profit;
-        payable(msg.sender).transfer(_amount - profit);
+        (bool success, ) = payable(msg.sender).call{value: _amount - profit}("");
+        require(success, TransferFailed());
+        depositTime[newCurrentId] = block.timestamp;
         emit Withdraw(msg.sender, _amount);
     }
-
 }
